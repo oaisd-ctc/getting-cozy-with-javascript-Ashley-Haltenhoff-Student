01a72b8d05ed73b84638ef64de83eb195a794878
AJ <144151461+Ashley-Haltenhoff-Student@users.noreply.github.com>
2024-09-09 18:44:29 +0000

    worked more within forms

--- FormHandlingAndValidation/scripts/app.js
+++ FormHandlingAndValidation/scripts/app.js
@@ -3,15 +3,36 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const age = document.getElementById('age');
 
-submitBtn.addEventListener('click', function(event) {
+submitBtn.addEventListener('click', () => {
     let canSubmit = true;
     
-    if (username.value.length <= 3) {
+    if (username.validity.patternMismatch || username.validity.valueMissing) {
         document.getElementById('username-error').classList.remove('invisible');
         canSubmit = false;
     }
+    else {
+        document.getElementById('username-error').classList.add('invisible');
+    }
 
-    if (age.value < 10 && age.value > 100) {
+    if (email.validity.patternMismatch || email.validity.valueMissing) {
+        document.getElementById('email-error').classList.remove('invisible');
+        canSubmit = false;
+    }
+    else {
+        document.getElementById('email-error').classList.add('invisible');
+    }
+
+
+    if (age.validity.rangeUnderflow || age.validity.valueMissing || age.validity.rangeUnderflow) {
         document.getElementById('age-error').classList.remove('invisible');
+        canSubmit = false;
+    }
+    else {
+        document.getElementById('age-error').classList.add('invisible');
+    }
+
+
+    if (canSubmit) {
+        alert("FORM SUBMITTED");
     }
 })
